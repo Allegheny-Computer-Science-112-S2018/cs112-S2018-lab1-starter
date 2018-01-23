ea5ceb7a0d2cdc194b6d26be3aa31cb433a624b7
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-01-23 10:32:10 -0500

    Improving the README and then updates to the gatorgrader script.

--- gatorgrader.sh
+++ gatorgrader.sh
@@ -186,7 +186,7 @@ if [ "$CHECK" = true ]; then
   python3 gatorgrader/gatorgrader.py --nowelcome --directories writing --checkfiles reflection.md --paragraphs 6 --sentences 3
   determine_exit_code $?
   # --> GatorGrader CHECK: the repository contains at least five commits
-  python3 gatorgrader/gatorgrader.py --nowelcome --commits 13
+  python3 gatorgrader/gatorgrader.py --nowelcome --commits 14
   determine_exit_code $?
  echo ""
   printf "%s\n" "${blu}... Finished checking with GatorGrader${end}"
